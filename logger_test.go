81eab19f9d0dadf1d1c7ec589fd1d65b4aee3263
binkkatal <ranbir@in2tive.ai>
2020-09-07 21:14:15 +0530

    Change test case dep import

--- logger_test.go
+++ logger_test.go
@@ -12,7 +12,7 @@ import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest/observer"
 
-	"github.com/wantedly/gorm-zap/testhelper"
+	"github.com/binkkatal/gorm-zap/testhelper"
 )
 
 var pool *testhelper.DockerPool
